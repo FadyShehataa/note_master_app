12120f1873751334ed8bdb4cf35d37a07c6b6579
FadyShehataa <fadyshehata40@gmail.com>
2023-10-17 16:53:11 +0300

    create note model

---
+++ lib/Feature/Home/data/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  String title;
+  String description;
+  int color;
+  String date;
+
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.color,
+    required this.date,
+  });
+}
